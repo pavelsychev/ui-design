466ecf1bdcbcb64d3a3e54f2b67bb416e9f8b1f6
Pavel Sychev <pavelsychev94@gmail.com>
2023-08-13 23:57:52 +0300

    Delete animation

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,6 @@
  /*Импорт chart.js*/
 import * as chartJS from "./modules/charts.js";
 
-import * as gsapJS from "./modules/gsap.js";
-
 /*Импорт select-pure.js*/
 import SelectPure from "select-pure";
 
@@ -45,21 +43,31 @@ function handleInputChange(e) {
   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
 }
 
-// rangeInputs.forEach(input => {
-//   input.addEventListener('input', handleInputChange)
-// })
-// // section misc, star rating
-// function initRatings() {
-//   let ratingActive, ratingValue;
 
-//   for (let index = 0; index < initRatings.length; index++) {
-//     const rating = ratings[index];
-//     initRatings(rating);
-//   }
+ // section misc, star rating
+ const rating_groups = [... document.getElementsByClassName('misc__rating rating__set')];
 
-//   function initRatings
-// }
-
+ rating_groups.forEach(rating_group => {
+   const starts = [... rating_group.getElementsByClassName('misc__rating-star')];
+   const rating_val = rating_group.getElementsByClassName('misc__rating-value')[0];
+ 
+   starts.forEach(star => {
+     let old_val = Number(rating_val.innerHTML);
+ 
+     star.addEventListener('click', () => {
+       old_val =  Number(star.value);
+       rating_val.innerHTML = old_val;
+     });
+ 
+     star.addEventListener('mouseover', () => {
+       rating_val.innerHTML = Number(star.value);
+     });
+ 
+     star.addEventListener('mouseout', () => {
+       rating_val.innerHTML = old_val;
+     });
+   });
+ });
 // section headers, burger
 let btnMenu = document.getElementsByClassName("btn-menu");
 let body = document.body;
