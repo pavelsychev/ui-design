9aa23674d26c5c6407ab36ab8ad3b5caa61996e1
Pavel Sychev <pavelsychev94@gmail.com>
2023-08-16 12:42:12 +0300

    app.js updated

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,16 @@ import * as chartJS from "./modules/charts.js";
 import SelectPure from "select-pure";
 
 
+
 //counter form
 const counter_forms = [... document.querySelectorAll('.form__counter')];
 
 counter_forms.forEach ( function (form) {
   const btn_plus = form.querySelector('.counter-plus');
   const btn_minus = form.querySelector('.counter-minus');
+
+  if (btn_plus === null || btn_minus === null)
+    return;
 
   btn_plus.addEventListener('click', function() {
     const counter_label_element = form.querySelector('.form__counter-quantity');
@@ -32,7 +36,6 @@ counter_forms.forEach ( function (form) {
     counter_label_element.innerHTML = count < 10 ? '0' + count : count;
   });
 });
-
 
 //section form, range
 const rangeInputs = document.querySelectorAll('input[type="range"]')
@@ -53,27 +56,27 @@ function handleInputChange(e) {
  // section misc, star rating
  const rating_groups = [... document.getElementsByClassName('misc__rating rating__set')];
 
- rating_groups.forEach(rating_group => {
-   const starts = [... rating_group.getElementsByClassName('misc__rating-star')];
-   const rating_val = rating_group.getElementsByClassName('misc__rating-value')[0];
- 
-   starts.forEach(star => {
-     let old_val = Number(rating_val.innerHTML);
- 
-     star.addEventListener('click', () => {
-       old_val =  Number(star.value);
-       rating_val.innerHTML = old_val;
-     });
- 
-     star.addEventListener('mouseover', () => {
-       rating_val.innerHTML = Number(star.value);
-     });
- 
-     star.addEventListener('mouseout', () => {
-       rating_val.innerHTML = old_val;
-     });
-   });
- });
+rating_groups.forEach(rating_group => {
+  const starts = [... rating_group.getElementsByClassName('misc__rating-star')];
+  const rating_val = rating_group.getElementsByClassName('misc__rating-value')[0];
+
+  starts.forEach(star => {
+    let old_val = Number(rating_val.innerHTML);
+
+    star.addEventListener('click', () => {
+      old_val =  Number(star.value);
+      rating_val.innerHTML = old_val;
+    });
+
+    star.addEventListener('mouseover', () => {
+      rating_val.innerHTML = Number(star.value);
+    });
+
+    star.addEventListener('mouseout', () => {
+      rating_val.innerHTML = old_val;
+    });
+  });
+});
 // section headers, burger
 let btnMenu = document.getElementsByClassName("btn-menu");
 let body = document.body;
